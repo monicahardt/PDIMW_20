ceee7eef154c66342e1d2ece483b49b1127eab8c
tayyab859 <tayyebyousafzai96@gmail.com>
2026-05-05 13:00:11 +0200

    Add University Effect prototype

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const repoName = 'PDIMW_20'
-
-export default defineConfig(({ mode }) => ({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  base: mode === 'production' ? `/${repoName}/` : '/',
-}))
+export default defineConfig({
+  plugins: [react()],
+});
